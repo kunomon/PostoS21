f091385e5c8ecbbeb4e10ab8515a25dd251dc6cb
JoseCostS <00001159568960sp@al.educacao.sp.gov.br>
2024-09-17 19:53:11 -0300

    Finalização Do Projeto

--- Etanol.py
+++
@@ -1,5 +0,0 @@
-from bomba_combustivel import BombaCombustivel
-
-class BombaEtanol(BombaCombustivel):
-    def __init__(self, valor_litro, quantidade_disponivel):
-        super().__init__(valor_litro, quantidade_disponivel)
